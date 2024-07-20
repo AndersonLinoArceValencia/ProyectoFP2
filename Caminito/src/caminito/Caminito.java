51d303747db83a134b23fdcc65fb21d901ba405a
AndersonArceValencia <aarceva@unsa.edu.pe>
2024-07-20 13:38:43 -0500

    Menu del Caminito
    Mathias mejoralo nose :b

---
+++ Caminito/src/caminito/Caminito.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caminito;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Caminito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+               CaminoGame game = new CaminoGame();
+        game.setVisible(true);
+    }
+    
+}
